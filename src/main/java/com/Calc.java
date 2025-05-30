2940ff48b9be417c0f230c62d9d25b4cfdc86abe
jiseopshin <wltjq1203@icloud.com>
2025-05-30 17:22:02 +0900

    테스트11

---
+++ src/main/java/com/Calc.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Calc {
+    public static int run(String exp) {
+        String[] expression = exp.split(" ");
+        int result = Integer.parseInt(expression[0]);
+        for(int i =1; i < expression.length; i+=2){
+            if (expression[i].equals("+")) {
+                result+=Integer.parseInt(expression[i+1]);
+            } else if (expression[i].equals("-")) {
+                result-=Integer.parseInt(expression[i+1]);
+            }
+        }
+        return result;
+    }
+}
